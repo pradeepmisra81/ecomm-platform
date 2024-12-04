c9b23f631e651b958f7c595640c8e009ca3f3ba3
pradeep <pradeep.misra@gmail.com>
2024-12-04 14:11:19 +0530

    Updated navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-20 sm:px-1 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
+      Navbar
+    </div>
   )
 }
 
