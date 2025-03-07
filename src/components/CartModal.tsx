8ae0e84b0afd16d41001b3c51da106e158178336
pradeep <pradeep.misra@gmail.com>
2025-03-07 20:51:53 +0530

    Updated navicons

---
+++ src/components/CartModal.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+const CartModal = () => {
+  return (
+    <div className="absolute ">CartModal</div>
+  )
+}
+
+export default CartModal
--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -20,12 +20,12 @@ const NavIcons = () => {
   }
 
   return (
-    <div className="flex items-center gap-4 xl:gap-6">
+    <div className="flex items-center gap-4 xl:gap-6 relative">
       <Image src="/profile.png" alt="" width={22} height={22} className="cursor_pointer" onClick={handleProfile} />
-      {isProfileOpen && <div className="">
+      {isProfileOpen && (<div className="absolute p-4 rounded-md top-12 left-0 text-sm shadow-[0_3px_10px_rgb(0,0,0,0,2)] z-20">
         <Link href="/profile">profile</Link>
         <div>Logout</div>
-      </div>}
+      </div>)}
       <Image src="/notification.png" alt="" width={22} height={22} className="cursor_pointer" />
       <Image src="/cart.png" alt="" width={22} height={22} className="cursor_pointer" />
     </div>
